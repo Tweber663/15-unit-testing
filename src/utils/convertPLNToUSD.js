0e9c2bb86a6fb5933bf11faf2a22b0081f9e0f79
Thomas Weber <tweber.1995@hotmail.com>
2024-04-17 20:16:49 +0200

    stimulating click action on component (last save of the day)

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,11 +1,19 @@
 export const convertPLNToUSD = (PLN) => {
 
-  const PLNtoUSD = PLN / 3.5;
-  
-  const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD'
-  });
-
-  return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+  if (typeof PLN === 'number' && PLN > 0 ) {
+    const PLNtoUSD = PLN / 3.5;
+    const formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    });
+    return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+  } else if (typeof PLN === 'string') {
+    return NaN
+  } else if (typeof PLN !== 'number' && PLN !== undefined ) {
+    return 'Error';
+  } else if (typeof PLN === 'number' && PLN < 0) {
+    return '$0.00';
+  } else {
+    return NaN
+  }
 }
